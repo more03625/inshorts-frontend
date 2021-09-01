c40bb64bfd6a7487d4a8305a668ac6eb7677ae1a
RAHUL MORE <more03625@gmail.com>
2021-09-01 20:18:59 +0530

    unwanted classed removed

--- src/components/pages/Author.js
+++ src/components/pages/Author.js
@@ -11,7 +11,7 @@ const Author = () => {
         <>
             <Header />
             <Breadcrumb />
-            <div className="rahul-more yogesh-more container-fluid pb-5 mb-2 mb-md-4">
+            <div className="container-fluid pb-5 mb-2 mb-md-4">
                 <div className="container-fluid">
                     <div className="row pt-5 mt-md-2">
                         <div className="col-lg-3"></div>
