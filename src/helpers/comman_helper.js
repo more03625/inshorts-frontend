c4a3eae53820986c8ac07ab6c82d440da24c92bc
rahulmore <rahulmore@worldindia.com>
2022-02-18 22:23:22 +0530

    host condition improved

--- src/helpers/comman_helper.js
+++ src/helpers/comman_helper.js
@@ -1,8 +1,7 @@
 import toast, { Toaster } from 'react-hot-toast';
 import React, { useHistory } from 'react';
 
-// export const Host = window.location.host === 'localhost:3000' ? 'http://localhost:5254' : 'https://newsdb-api.herokuapp.com';
-export const Host = window.location.host === 'localhost:3000' ? 'https://newsdb-api.herokuapp.com' : 'https://newsdb-api.herokuapp.com';
+export const Host = window.location.host === 'reactjs-newsdb.netlify.app' ? 'https://newsdb-api.herokuapp.com' : 'http://localhost:5254';
 
 export const Endpoints = {
     news: '/news',
